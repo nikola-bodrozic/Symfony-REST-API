d682d186573d1329b22e8bd83756d16e69ed1953
Nikola Bodrozic <nikola.bodrozic@example.com>
2018-02-28 13:55:09 +0100

    updated PHPUnit tests

--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase {
 
-    private $tuser = ["name" => "phpunit-mike", "role" => "manager"];
+    private $tuser = ["name" => "phpunit-mike2", "role" => "manager"];
     private $uid;
     private $usedid = array();
 
@@ -18,10 +18,13 @@ class UserControllerTest extends WebTestCase {
                 array(), 
                 array(), 
                 array(
-                    'CONTENT_TYPE' => 'application/json'), 
-                    json_encode($this->tuser)
+                    'CONTENT_TYPE' => 'application/json', 
+                    "HTTP_AUTHORIZATION" => "Bearer e4d3"
+                ), 
+                json_encode($this->tuser)
         );
         $data = json_decode($client->getResponse()->getContent(), true);
+        //print_r($data);die;
         $this->uid = $data['id'];
         array_push($this->usedid, $data['id']);
     }
@@ -34,7 +37,10 @@ class UserControllerTest extends WebTestCase {
                     '/user/' . $this->uid,
                     array(), 
                     array(), 
-                    array()
+                    array(
+                        'CONTENT_TYPE' => 'application/json', 
+                        "HTTP_AUTHORIZATION" => "Bearer e4d3"
+                    )
             );            
         }
     }
@@ -42,10 +48,11 @@ class UserControllerTest extends WebTestCase {
     public function testGetUser()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/user');
+        $crawler = $client->request('GET', '/user', [], [], []);
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $data = json_decode($client->getResponse()->getContent(), true);
         $arr_index = count($data)-1;
+        //error_log("---" . __METHOD__ . " : " . __LINE__  . "\n" . print_r($data, TRUE), 0);        
         $this->assertEquals($data[$arr_index]['name'], $this->tuser['name']);
         $this->assertEquals($data[$arr_index]['role'], $this->tuser['role']);        
     }
@@ -57,7 +64,7 @@ class UserControllerTest extends WebTestCase {
         $data = json_decode($client->getResponse()->getContent(), true);
         $this->assertEquals($data['name'], $this->tuser['name']);
         $this->assertEquals($data['role'], $this->tuser['role']);
-
+        //error_log("---" . __METHOD__ . " : " . __LINE__  . "\n" . print_r($data, TRUE), 0);  
         $client = static::createClient();
         $crawler = $client->request('GET', '/user/1345345345345');
         $this->assertEquals(404, $client->getResponse()->getStatusCode());
@@ -70,7 +77,10 @@ class UserControllerTest extends WebTestCase {
                 '/user/' . $this->uid, 
                 array(), 
                 array(), 
-                array()
+                array(
+                    'CONTENT_TYPE' => 'application/json', 
+                    "HTTP_AUTHORIZATION" => "Bearer e4d3"
+                )
         );
         array_pop($this->usedid);
         $this->assertEquals(200, $client->getResponse()->getStatusCode()); 
@@ -83,7 +93,10 @@ class UserControllerTest extends WebTestCase {
                 '/user/' . $this->uid , 
                 array(), 
                 array(), 
-                array('CONTENT_TYPE' => 'application/json'), 
+                array(
+                    'CONTENT_TYPE' => 'application/json', 
+                    "HTTP_AUTHORIZATION" => "Bearer e4d3"
+                ),
                 json_encode(["name" => "phpunit-test-update", "role" => "manager"])
         );
         $this->assertEquals(200, $client->getResponse()->getStatusCode()); 
